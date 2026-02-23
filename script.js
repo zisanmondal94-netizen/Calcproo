9992f013ae66280a9f827504a6baff33c3ff68b6
zisanmondal94-netizen <zisanmondal94@gmail.com>
2026-02-24 00:36:19 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+let display = document.getElementById("display");
+
+function appendValue(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Error";
+    }
+}
+
+function toggleTheme() {
+    document.body.classList.toggle("dark");
+          }
